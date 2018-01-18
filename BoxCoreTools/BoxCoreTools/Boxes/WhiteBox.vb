de5ea77804f8f3c087585f0b026dc9e5ef9049b0
Angel Andres <ggeelloo@gmail.com>
2018-01-18 12:18:24 +0100

    Added whitebox

---
+++ BoxCoreTools/BoxCoreTools/Boxes/WhiteBox.vb
@@ -0,0 +1,9 @@
+﻿Public Class WhiteBox
+  Public Property CollectorManager As New CollectorManager()
+  Public Property Encoder As New Object
+  Public Property TxChainBridgeManager As New Object
+
+  Public Sub New()
+
+  End Sub
+End Class
